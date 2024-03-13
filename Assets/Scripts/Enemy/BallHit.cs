718b8f08cc35af07c081999b2948e3ef74bb8954
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2024-03-13 14:55:18 -0700

    fixed errors

--- Assets/Scripts/Enemy/BallHit.cs
+++ Assets/Scripts/Enemy/BallHit.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using UnityEngine;
 
-public class BallHit : MonoBehaviour {
+public class BallHit : MonoBehaviour
+{
+    private GameManager _gameManager;
 
-}
+    private void Awake()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         //Debug.Log("ball collided with: " + collision.gameObject.name + " tag: " + collision.gameObject.tag);
         if (collision.gameObject.CompareTag("Player") && !_gameManager.CurrentState.playerAttacked) // avoid causing player more dammage if player already taking damage
         {
             _gameManager.CurrentState.playerAttacked = true;
-            Destroy(gameObject)
+            Destroy(gameObject);
         }
     }
+}
